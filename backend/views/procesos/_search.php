cdf688db2571eabc58f20520236c5b558b80d7ef
Leonardo Javier Alviarez Hernández <alviarez.leonardo@gmail.com>
2016-10-04 09:30:00 -0400

    Generado CRUD para el modelo Procesos

---
+++ backend/views/procesos/_search.php
@@ -0,0 +1,35 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model backend\models\ProcesosSearch */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="procesos-search">
+
+    <?php $form = ActiveForm::begin([
+        'action' => ['index'],
+        'method' => 'get',
+    ]); ?>
+
+    <?= $form->field($model, 'id') ?>
+
+    <?= $form->field($model, 'codigo') ?>
+
+    <?= $form->field($model, 'nombre') ?>
+
+    <?= $form->field($model, 'fecha_inicio') ?>
+
+    <?= $form->field($model, 'fecha_fin') ?>
+
+    <div class="form-group">
+        <?= Html::submitButton(Yii::t('app', 'Search'), ['class' => 'btn btn-primary']) ?>
+        <?= Html::resetButton(Yii::t('app', 'Reset'), ['class' => 'btn btn-default']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
