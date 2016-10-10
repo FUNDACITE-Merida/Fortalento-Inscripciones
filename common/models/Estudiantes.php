7766bb999d264ed39a3f5e750fa86c77718f35f0
Leonardo Javier Alviarez Hernández <alviarez.leonardo@gmail.com>
2016-10-10 15:53:53 -0400

    Adecuado el modelo Estudiantes

--- common/models/Estudiantes.php
+++ common/models/Estudiantes.php
@@ -43,6 +43,7 @@ class Estudiantes extends \yii\db\ActiveRecord
 			[['fecha_nacimiento'], 'date', 'max' => Yii::$app->formatter->asDate('now')],
             [['es_venezolano'], 'boolean'],
             [['cedula'], 'string', 'max' => 8, 'tooLong' => '{attribute} deberia contener máximo 8 números'],
+            [['cedula'], 'match', 'pattern' => '/^[0-9]*$/'],
             [['nombre', 'apellido', 'lugar_nacimiento'], 'string', 'max' => 256],
             [['genero'], 'string', 'max' => 1],
             [['cedula'], 'unique'],
@@ -65,14 +66,15 @@ class Estudiantes extends \yii\db\ActiveRecord
     {
         return [
             'id' => Yii::t('app', 'Clave primaria, autoincremental'),
-            'cedula' => Yii::t('app', 'Cédula de identidad del estudiantes'),
-            'nombre' => Yii::t('app', 'Nombre del estudiante'),
-            'apellido' => Yii::t('app', 'Apellido del estudiante'),
-            'fecha_nacimiento' => Yii::t('app', 'Fecha de nacimiento del estudiante en timestamp'),
-            'lugar_nacimiento' => Yii::t('app', 'Lugar de nacimiento del estudiante'),
-            'genero' => Yii::t('app', 'Género del estudiante, M: Masculino, f: Femenino'),
-            'es_venezolano' => Yii::t('app', 'Especifica si el estudiante es venezolano o extranjero'),
+            'cedula' => Yii::t('app', 'Cédula de identidad'),
+            'nombre' => Yii::t('app', 'Primer nombre e inicial del segundo'),
+            'apellido' => Yii::t('app', 'Primer apellido e inicial del segundo'),
+            'fecha_nacimiento' => Yii::t('app', 'Fecha de nacimiento'),
+            'lugar_nacimiento' => Yii::t('app', 'Lugar de nacimiento'),
+            'genero' => Yii::t('app', 'Sexo'),
+            'es_venezolano' => Yii::t('app', 'Nacionalidad'),
             'id_user' => Yii::t('app', 'Id del usuario que registró al estudiante'),
+			'no_cedula' => Yii::t('app', 'No tengo cédula'),
         ];
     }
 
@@ -91,6 +93,16 @@ class Estudiantes extends \yii\db\ActiveRecord
     {
         return $this->hasMany(Inscripciones::className(), ['id_estudiante' => 'id']);
     }
+    
+    /**
+     * by LJAH
+     * Relación con el modelo User
+     * @return \yii\db\ActiveQuery
+     */
+    public function getUser()
+    {
+        return $this->hasOne(User::className(), ['id' => 'id_user']);
+    }
 
     /**
      * @inheritdoc
@@ -100,4 +112,64 @@ class Estudiantes extends \yii\db\ActiveRecord
     {
         return new EstudiantesQuery(get_called_class());
     }
+    
+    public static function getEstudianteUser()
+    {
+		return Estudiantes::findOne(['id_user' => Yii::$app->user->id]);
+	}
+	
+	/*
+	 * Retorna el modelo Inscripcion del estudiante actual
+	 */
+	public function getInscripcion()
+	{
+		return Inscripciones::findOne([
+						'id_estudiante' => $this->id,
+						'id_proceso' => Procesos::getIdProcesoAbierto(),
+				]);
+	}
+	
+	/*
+	 * Retorna el modelo EstudioSocioEconomico del estudiante actual
+	 */
+	public function getEstudioSocioEconomico()
+	{
+		return EstudioSocioEconomico::findOne([
+						'id_estudiante' => $this->id,
+						'id_proceso' => Procesos::getIdProcesoAbierto(),
+				]);
+	}
+	
+	
+	public function afterFind()
+	{
+		if (!$this->es_venezolano)
+			$this->es_venezolano = 0;
+		
+		if (empty($this->cedula))
+			$this->no_cedula = true;
+		// Formatea la fecha según se ha configurado en config/web.php
+		// 'formatter' => [
+        //	   'dateFormat' => 'dd-MM-yyyy',
+        // ]
+            
+		$this->fecha_nacimiento = Yii::$app->formatter->asDate($this->fecha_nacimiento);
+		
+		parent::afterFind();
+	}
+	
+	public function beforeSave()
+	{
+		if ($this->no_cedula)
+		{
+			//$this->cedula = '00000'.rand(1, 999);
+			//echo $this->validate(array('cedula'));
+			do
+			{
+				//$this->cedula = '00000'.rand(1, 999);
+				$this->cedula = '00000'.rand(1, 999);
+			}while (!($this->validate(array('cedula'))));
+		}
+		return true;
+	}
 }
