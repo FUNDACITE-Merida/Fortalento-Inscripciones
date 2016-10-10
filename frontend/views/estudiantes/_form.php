d47e34804623cb4495a8376fc87c659f3964ccb2
Leonardo Javier Alviarez Hernández <alviarez.leonardo@gmail.com>
2016-10-10 15:46:49 -0400

    Adecuación de la vista Datos de Estudiante

--- frontend/views/estudiantes/_form.php
+++ frontend/views/estudiantes/_form.php
@@ -2,36 +2,73 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+use yii\web\View;
 
 /* @var $this yii\web\View */
 /* @var $model common\models\Estudiantes */
 /* @var $form yii\widgets\ActiveForm */
+
+$this->registerJs("
+$('#estudiantes-no_cedula').change(function() {
+			if ($(this).is(':checked')) 
+			{
+				$('#estudiantes-cedula').prop('readonly', true);
+			}
+			else
+			{
+				$('#estudiantes-cedula').prop('readonly', false);
+			}			
+		});
+", 
+View::POS_READY, 
+'my-options');
+
+$sincedula = false;
+if ($model->no_cedula == true)
+	$sincedula = true;
 ?>
 
 <div class="estudiantes-form">
 
     <?php $form = ActiveForm::begin(); ?>
 
-    <?= $form->field($model, 'cedula')->textInput(['maxlength' => true]) ?>
+	<div class="row">
+	  <div class="col-lg-5 col-md-10">
+		<?= $form->field($model, 'apellido')->textInput(['maxlength' => true])->hint('Escriba el primer apellido e inicial del segundo') ?>
+	  </div>
+	  <div class="col-lg-5 col-md-10">
+		<?= $form->field($model, 'nombre')->textInput(['maxlength' => true])->hint('Escriba el primer nombre e inicial del segundo') ?>
+	  </div>	  
+	</div>
+    
+    <div class="row">
+	  <div class="col-lg-5 col-md-10">
+		  <?= $form->field($model, 'cedula')->textInput(['maxlength' => true, 'readonly' => $sincedula]) ?>
+		  <?= $form->field($model, 'no_cedula')->checkbox() ?>
+		
+	  </div>
+	  <div class="col-lg-5 col-md-10">
+		<?= $form->field($model, 'es_venezolano')->radioList(['1' => 'Venezolana', '0' => 'Extranjera']) ?>
+	  </div>	  
+	</div>
+    
+	<div class="row">
+	  <div class="col-lg-5 col-md-10">
+		<?= $form->field($model, 'fecha_nacimiento')->textInput()->hint('Ejemplo: 16-07-1980') ?>
+	  </div>
+	  <div class="col-lg-5 col-md-10">
+		<?= $form->field($model, 'lugar_nacimiento')->textInput(['maxlength' => true])?>
+	  </div>	  
+	</div>
+	
+    <div class="row">
+	  <div class="col-lg-5 col-md-10">
+		<?= $form->field($model, 'genero')->radioList(['F' => 'Femenino', 'M' => 'Masculino']) ?>
+	  </div>
+	</div>
 
-    <?= $form->field($model, 'nombre')->textInput(['maxlength' => true]) ?>
-
-    <?= $form->field($model, 'apellido')->textInput(['maxlength' => true]) ?>
-
-    <?= $form->field($model, 'fecha_nacimiento')->textInput() ?>
-
-    <?= $form->field($model, 'lugar_nacimiento')->textInput(['maxlength' => true]) ?>
-
-    <?= $form->field($model, 'genero')->textInput(['maxlength' => true]) ?>
-
-    <?= $form->field($model, 'es_venezolano')->checkbox() ?>
-
-    <?= $form->field($model, 'id_user')->textInput() ?>
-
-    <div class="form-group">
-        <?= Html::submitButton($model->isNewRecord ? Yii::t('app', 'Create') : Yii::t('app', 'Update'), ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
-    </div>
-
-    <?php ActiveForm::end(); ?>
-
+  <div class="form-group">
+      <?= Html::submitButton('Siguiente <span class="glyphicon glyphicon-arrow-right"></span>', ['class' => 'btn btn-primary']) ?>
+  </div>
+  <?php ActiveForm::end(); ?>
 </div>
--- frontend/views/estudiantes/create.php
+++ frontend/views/estudiantes/create.php
@@ -6,7 +6,7 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 /* @var $model common\models\Estudiantes */
 
-$this->title = Yii::t('app', 'Create Estudiantes');
+$this->title = Yii::t('app', 'Datos del Estudiante');
 $this->params['breadcrumbs'][] = ['label' => Yii::t('app', 'Estudiantes'), 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
