09e29b8fb064204e70cc8e5950f97199ff05203c
Leonardo Javier Alviarez Hernández <alviarez.leonardo@gmail.com>
2016-10-13 10:27:09 -0400

    Agregado modelo NseProfesionJefeFamilia

---
+++ common/models/NseProfesionJefeFamilia.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace common\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "nse_profesion_jefe_familia".
+ *
+ * @property integer $cod_prof_jf
+ * @property string $descripcion
+ *
+ * @property Aludati[] $aludatis
+ */
+class NseProfesionJefeFamilia extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'nse_profesion_jefe_familia';
+    }
+
+    /**
+     * @return \yii\db\Connection the database connection used by this AR class.
+     */
+    public static function getDb()
+    {
+        return Yii::$app->get('fortalento');
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['descripcion'], 'required'],
+            [['descripcion'], 'string', 'max' => 50]
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'cod_prof_jf' => 'Cod Prof Jf',
+            'descripcion' => 'Descripcion',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getAludatis()
+    {
+        return $this->hasMany(Aludati::className(), ['cod_prof_jf' => 'cod_prof_jf']);
+    }
+
+    /**
+     * @inheritdoc
+     * @return NseProfesionJefeFamiliaQuery the active query used by this AR class.
+     */
+    public static function find()
+    {
+        return new NseProfesionJefeFamiliaQuery(get_called_class());
+    }
+}
