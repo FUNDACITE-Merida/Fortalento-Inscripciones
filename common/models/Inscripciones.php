b923740c3a12ba88a9e6e76e8656aaf0b67f5293
Leonardo Javier Alviarez Hernández <alviarez.leonardo@gmail.com>
2016-10-13 11:32:17 -0400

    Adecuación del modelo Inscripciones

--- common/models/Inscripciones.php
+++ common/models/Inscripciones.php
@@ -28,11 +28,13 @@ use Yii;
  * @property integer $codigo_grupo_familiar
  * @property boolean $cerrada
  *
+ * @property EstudioSocioEconomico[] $estudioSocioEconomicos
  * @property Estudiantes $idEstudiante
  * @property Procesos $idProceso
  */
 class Inscripciones extends \yii\db\ActiveRecord
 {
+	private $_patronNumero = '/^\s*[-+]?[0-9]*\,?[0-9]+([eE][-+]?[0-9]+)?\s*$/';
     /**
      * @inheritdoc
      */
@@ -48,14 +50,94 @@ class Inscripciones extends \yii\db\ActiveRecord
     {
         return [
             [['id_proceso', 'id_estudiante', 'fecha_inscripcion', 'codigo_plantel', 'localidad_plantel', 'codigo_ultimo_grado', 'postulado_para_beca', 'postulado_para_premio', 'codigo_profesion_jefe_familia', 'codigo_nivel_instruccion_madre', 'codigo_fuente_ingreso_familia', 'codigo_vivienda_familia', 'codigo_ingreso_familia', 'codigo_grupo_familiar'], 'required'],
-            [['id_proceso', 'id_estudiante', 'fecha_inscripcion', 'codigo_profesion_jefe_familia', 'codigo_nivel_instruccion_madre', 'codigo_fuente_ingreso_familia', 'codigo_vivienda_familia', 'codigo_ingreso_familia', 'codigo_grupo_familiar'], 'integer'],
-            [['promedio', 'nota1', 'nota2', 'nota3'], 'number'],
+            [['id_proceso', 'id_estudiante','codigo_profesion_jefe_familia', 'codigo_nivel_instruccion_madre', 'codigo_fuente_ingreso_familia', 'codigo_vivienda_familia', 'codigo_ingreso_familia', 'codigo_grupo_familiar'], 'integer'],
+            
             [['cerrada'], 'boolean'],
             [['codigo_plantel'], 'string', 'max' => 10],
-            [['localidad_plantel', 'postulado_para_beca', 'postulado_para_premio'], 'string', 'max' => 256],
+            [['localidad_plantel'], 'string', 'max' => 256],
             [['codigo_ultimo_grado'], 'string', 'max' => 4],
+            [['postulado_para_beca'], 'string', 'max' => 1],
+            [['postulado_para_premio'], 'string', 'max' => 1],
             [['id_estudiante'], 'exist', 'skipOnError' => true, 'targetClass' => Estudiantes::className(), 'targetAttribute' => ['id_estudiante' => 'id']],
             [['id_proceso'], 'exist', 'skipOnError' => true, 'targetClass' => Procesos::className(), 'targetAttribute' => ['id_proceso' => 'id']],
+            
+            ['promedio', 'number', 
+				'numberPattern' => $this->_patronNumero,
+				'min' => 10, 'max' => 20,
+				'when' => function ($model) {
+							return $model->postulado_para_beca == true;
+				}, 'whenClient' => "function (attribute, value) {
+						return $('#inscripciones-postulado_para_beca').is(':checked') == true;
+					}"
+			],
+			
+			[['nota1', 'nota2'], 'number', 
+				'numberPattern' => $this->_patronNumero,
+				'min' => 10, 'max' => 20,
+				'when' => function ($model) {
+							return $model->postulado_para_premio == true;
+				}, 'whenClient' => "function (attribute, value) {
+						return $('#inscripciones-postulado_para_premio').is(':checked') == true;
+					}"
+			],
+			
+			['promedio', 'required', 
+				'when' => function ($model) {
+							return $model->postulado_para_beca == true;
+				}, 'whenClient' => "function (attribute, value) {
+						return $('#inscripciones-postulado_para_beca').is(':checked') == true;
+					}"
+			],
+			
+			[['nota1', 'nota2'], 'required',
+				'when' => function ($model) {
+							return $model->postulado_para_premio == true;
+				}, 'whenClient' => "function (attribute, value) {
+						return $('#inscripciones-postulado_para_premio').is(':checked') == true;
+					}"
+			],
+			
+			/* Validación especial para nota3 ya que hay algunas restricciones cuando el 
+			 * estudiante está cursando 5to año en un Liceo que no gradúa 6to año sino 
+			 * únicamente hasta 5to año
+			 */
+			 
+			 [['nota3'], 'number', 
+				'numberPattern' => $this->_patronNumero,
+				'min' => 10, 'max' => 20,
+				'when' => function ($model) {
+							return ($model->postulado_para_premio == true && $model->codigo_ultimo_grado != 11);
+				}, 'whenClient' => "function (attribute, value) {
+						return ($('#inscripciones-postulado_para_premio').is(':checked') == true && $('#inscripciones-codigo_ultimo_grado').val != 11);
+					}"
+			],
+						
+			[['nota3'], 'required',
+				'when' => function ($model) {
+							return ($model->postulado_para_premio == true && $model->codigo_ultimo_grado != 11);
+				}, 'whenClient' => "function (attribute, value) {
+						return ($('#inscripciones-postulado_para_premio').is(':checked') == true && $('#inscripciones-codigo_ultimo_grado').val != 11);
+					}"
+			],
+			 /***********************/ 
+			 
+			
+			[['postulado_para_beca'], 'required', 'requiredValue' => 'B', 'message' => 'Debe seleccionar al menos una postulación',
+				'when' => function ($model) {
+							return $model->postulado_para_premio == false;
+				}, 'whenClient' => "function (attribute, value) {
+						return $('#inscripciones-postulado_para_premio').is(':checked') == false;
+					}"
+			],
+			
+			[['postulado_para_premio'], 'required', 'requiredValue' => 'P', 'message' => 'Debe seleccionar al menos una postulación',
+				'when' => function ($model) {
+							return $model->postulado_para_beca == false;
+				}, 'whenClient' => "function (attribute, value) {
+						return $('#inscripciones-postulado_para_beca').is(':checked') == false;
+					}"
+			],
+			
         ];
     }
 
@@ -68,24 +150,33 @@ class Inscripciones extends \yii\db\ActiveRecord
             'id' => Yii::t('app', 'Clave primaria, autoincremental. Este id servirá a la vez como número de planilla de inscripción a efecto de los reportes. No se creo un campo especial para el número de planilla ya que en la práctica cumpliría la misma función que está haciendo el id.'),
             'id_proceso' => Yii::t('app', 'Clave foránea que referencia a la tabla procesos'),
             'id_estudiante' => Yii::t('app', 'Clave foránea que referencia a la tabla estudiantes'),
-            'fecha_inscripcion' => Yii::t('app', 'Fecha en que se realiza la inscripción en timestamp'),
-            'codigo_plantel' => Yii::t('app', 'Código del plantel de donde proviene el estudiante'),
-            'localidad_plantel' => Yii::t('app', 'Localidad del plantel de donde proviene el estudiante'),
-            'codigo_ultimo_grado' => Yii::t('app', 'Código del último grado cursado por el estudiante'),
-            'postulado_para_beca' => Yii::t('app', 'Almacena el valor de la postulación del estudiante, B para Beca'),
-            'postulado_para_premio' => Yii::t('app', 'Almacena el valor de la postulación del estudiante, P para Premio'),
-            'promedio' => Yii::t('app', 'Promedio de notas del último año/grado cursado por el estudiante'),
-            'nota1' => Yii::t('app', '1ero de los promedios de los tres últimos años cursados por el estudiante'),
-            'nota2' => Yii::t('app', '2do de los promedios de los tres últimos años cursados por el estudiante'),
-            'nota3' => Yii::t('app', '3ero de los promedios de los tres últimos años cursados por el estudiante'),
-            'codigo_profesion_jefe_familia' => Yii::t('app', 'Número que identifica la profesión del jefe de familia'),
-            'codigo_nivel_instruccion_madre' => Yii::t('app', 'Número que identifica el nivel de instrucción de la madre'),
-            'codigo_fuente_ingreso_familia' => Yii::t('app', 'Número que identifica la fuente de ingreso familiar'),
-            'codigo_vivienda_familia' => Yii::t('app', 'Número que identifica el tipo de vivienda de la familia'),
-            'codigo_ingreso_familia' => Yii::t('app', 'Número que identifica el monto de ingreso familiar'),
-            'codigo_grupo_familiar' => Yii::t('app', 'Número que identifica la cantidad de personas que conforman el grupo familiar'),
-            'cerrada' => Yii::t('app', 'Si está en True la inscripción ha sido cerrada, si está en False la inscripción está abierta. Una inscripción se considera cerrada si el usuario ha llenado todos los campos requeridos para la inscripción.'),
+            'fecha_inscripcion' => Yii::t('app', 'Fecha de solicitud'),
+            'codigo_plantel' => Yii::t('app', 'Seleccione el plantel'),
+            'localidad_plantel' => Yii::t('app', 'Localidad del plantel'),
+            'codigo_ultimo_grado' => Yii::t('app', 'Último año/grado culminado'),
+            'postulado_para_beca' => Yii::t('app', 'Premio beca-estímulo al alto rendimiento estudiantil'),
+            'postulado_para_premio' => Yii::t('app', 'Premio de reconocimiento a la excelencia'),
+            'promedio' => Yii::t('app', 'Promedio de notas'),
+            'nota1' => Yii::t('app', 'Promedio global 1'),
+            'nota2' => Yii::t('app', 'Promedio global 2'),
+            'nota3' => Yii::t('app', 'Promedio global 3'),
+            'codigo_profesion_jefe_familia' => Yii::t('app', 'Profesión del jefe de familia'),
+            'codigo_nivel_instruccion_madre' => Yii::t('app', 'Nivel de instrucción de la madre'),
+            'codigo_fuente_ingreso_familia' => Yii::t('app', 'Fuente de ingreso familiar'),
+            'codigo_vivienda_familia' => Yii::t('app', 'Alojamiento y vivienda'),
+            'codigo_ingreso_familia' => Yii::t('app', 'Ingreso familiar (sin deducciones)'),
+            'codigo_grupo_familiar' => Yii::t('app', 'Grupo familiar'),
+            'cerrada' => Yii::t('app', 'Inscripción cerrada'),
+			'cedula' => Yii::t('app', 'Cédula de identidad'),
         ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getEstudioSocioEconomico()
+    {
+        return $this->hasOne(EstudioSocioEconomico::className(), ['n_planilla_inscripcion' => 'id']);
     }
 
     /**
@@ -103,6 +194,15 @@ class Inscripciones extends \yii\db\ActiveRecord
     {
         return $this->hasOne(Procesos::className(), ['id' => 'id_proceso']);
     }
+    
+    /**
+     * by LJAH
+     * @return \yii\db\ActiveQuery
+     */
+    public function getCodPlantel()
+    {
+        return $this->hasOne(Plantel::className(), ['cod_pla' => 'codigo_plantel']);
+    }
 
     /**
      * @inheritdoc
@@ -112,4 +212,90 @@ class Inscripciones extends \yii\db\ActiveRecord
     {
         return new InscripcionesQuery(get_called_class());
     }
+    
+    /**
+     * by LJAH
+     * Getter para realizar búsquedas por número de cédula del estudiante
+     * return cédula de estudiante
+     */
+     /*
+    public function getCedulaEstudiante()
+    {
+		return $this->idEstudiante->cedula;
+	}*/
+    
+    public function beforeSave()
+    {
+		$this->promedio = str_replace(',','.',$this->promedio);
+		$this->promedio = Yii::$app->formatter->asDecimal(floatval($this->promedio), 3);
+		
+		$this->nota1 = str_replace(',','.',$this->nota1);
+		$this->nota1 = Yii::$app->formatter->asDecimal(floatval($this->nota1), 3);
+		$this->nota2 = str_replace(',','.',$this->nota2);
+		$this->nota2 = Yii::$app->formatter->asDecimal(floatval($this->nota2), 3);
+		$this->nota3 = str_replace(',','.',$this->nota3);
+		$this->nota3 = Yii::$app->formatter->asDecimal(floatval($this->nota3), 3);
+		
+		return true;
+	}
+    
+    public function afterFind()
+	{
+		
+		// Formatea la fecha según se ha configurado en config/web.php
+		// 'formatter' => [
+        //	   'dateFormat' => 'dd-MM-yyyy',
+        // ]            
+		$this->fecha_inscripcion = Yii::$app->formatter->asDate($this->fecha_inscripcion);
+		
+		$this->promedio = str_replace('.',',',$this->promedio);
+		$this->nota1 = str_replace('.',',',$this->nota1);
+		$this->nota2 = str_replace('.',',',$this->nota2);
+		$this->nota3 = str_replace('.',',',$this->nota3);
+				
+		parent::afterFind();
+	}
+	
+	public function getEdadEstudiante() {
+		list($d,$m,$Y) = explode("-",$this->idEstudiante->fecha_nacimiento);
+		return( date("md") < $m.$d ? date("Y")-$Y-1 : date("Y")-$Y );
+	}
+	
+	public function estatusInscripcion()
+    {		
+		return $this->cerrada;
+	}
+	
+	public function getConsolidadoMunicipios()
+	{
+		/*
+		 * Acá se puede mejorar el tiempo de las consultas
+		 * usando la relación creada en los modelos: inscripciones <-> planteles <-> municipios
+		 */
+		$municipios = Municipios::find()->all();
+		
+		foreach($municipios as $municipio)
+		{
+			$planteles = $municipio->plantels;
+			$cerradas = 0;
+			$abiertas = 0;
+			foreach($planteles as $plantel)
+			{
+				$cerradas += Inscripciones::find()
+										->where(['codigo_plantel' => $plantel->cod_pla])
+										->andWhere(['cerrada' => true])
+										->count();
+										
+				$abiertas += Inscripciones::find()
+										->where(['codigo_plantel' => $plantel->cod_pla])
+										->andWhere(['cerrada' => false])
+										->count();
+			}
+			$datos[$municipio->municipio]['nombre'] = $municipio->municipio;
+			$datos[$municipio->municipio]['cerradas'] = $cerradas;
+			$datos[$municipio->municipio]['abiertas'] = $abiertas;
+		}
+		
+		return $datos;
+	}
 }
