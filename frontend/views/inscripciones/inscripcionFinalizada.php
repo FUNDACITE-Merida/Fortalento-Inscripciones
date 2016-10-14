c971d972af88c29d1d9f899c00bbf281b4e4ba74
Leonardo Javier Alviarez Hernández <alviarez.leonardo@gmail.com>
2016-10-14 14:41:46 -0400

    Agregada view

---
+++ frontend/views/inscripciones/inscripcionFinalizada.php
@@ -0,0 +1,22 @@
+<?php
+use yii\helpers\Html;
+use yii\web\View;
+
+$this->title="Inscripción Finalizada";
+
+$urlImprimir = Yii::$app->urlManager->createUrl(['/reportes/inscripcion']);
+$this->registerJS("
+	var url = '".$urlImprimir."';
+    window.location.href = url;
+",View::POS_LOAD);
+?>
+<h2>Ha finalizado su inscripción con éxito.</h2>
+<p>En breves momentos podrá descargar las planillas de inscripción.</p>
+<?= Html::a('<span class=".glyphicon .glyphicon-log-out"></span> Salir del sistema', 
+			['/site/logout'], 
+			['class' => 'btn btn-primary', 'role' => 'button',
+			'data' => [
+                //'confirm' => "Are you sure you want to delete profile?",
+                'method' => 'post',
+				],
+            ]) ?>
