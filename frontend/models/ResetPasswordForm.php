45a66fac10cc1ab841291749f9d6cacd20071317
Leonardo Javier Alviarez Hernández <alviarez.leonardo@gmail.com>
2016-10-17 15:06:32 -0400

    Adecuado el modelo y la vista de reinicio de password

--- frontend/models/ResetPasswordForm.php
+++ frontend/models/ResetPasswordForm.php
@@ -4,6 +4,7 @@ namespace frontend\models;
 use yii\base\Model;
 use yii\base\InvalidParamException;
 use common\models\User;
+use Yii;
 
 /**
  * Password reset form
--- frontend/views/site/resetPassword.php
+++ frontend/views/site/resetPassword.php
@@ -7,22 +7,22 @@
 use yii\helpers\Html;
 use yii\bootstrap\ActiveForm;
 
-$this->title = 'Reset password';
+$this->title = 'Nueva contraseña';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="site-reset-password">
     <h1><?= Html::encode($this->title) ?></h1>
 
-    <p>Please choose your new password:</p>
+    <p>Por favor ingresa tu nueva contraseña:</p>
 
     <div class="row">
         <div class="col-lg-5">
             <?php $form = ActiveForm::begin(['id' => 'reset-password-form']); ?>
 
-                <?= $form->field($model, 'password')->passwordInput(['autofocus' => true]) ?>
+                <?= $form->field($model, 'password')->passwordInput(['autofocus' => true])->label('Contraseña') ?>
 
                 <div class="form-group">
-                    <?= Html::submitButton('Save', ['class' => 'btn btn-primary']) ?>
+                    <?= Html::submitButton('Guardar', ['class' => 'btn btn-primary']) ?>
                 </div>
 
             <?php ActiveForm::end(); ?>
