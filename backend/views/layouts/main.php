eb4a7c2fdf336e8660588ca0f2377b7753bf38a6
Leonardo Javier Alviarez Hernández <alviarez.leonardo@gmail.com>
2016-10-28 02:59:14 +0000

    Agregado menú de de administración de usuarios y roles

--- backend/views/layouts/main.php
+++ backend/views/layouts/main.php
@@ -35,7 +35,7 @@ AppAsset::register($this);
         ],
     ]);
     $menuItems = [
-        ['label' => 'Home', 'url' => ['/site/index']],
+        ['label' => 'Usuarios/Roles', 'url' => ['/admin'], 'visible' => Yii::$app->user->can('superadmin')],
     ];
     if (Yii::$app->user->isGuest) {
         $menuItems[] = ['label' => 'Login', 'url' => ['/site/login']];
